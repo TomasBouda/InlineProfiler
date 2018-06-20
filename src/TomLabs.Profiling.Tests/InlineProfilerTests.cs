d8907f4c27766abc1394d7ba6def74b0a11350e8
Tommy <email@tomasbouda.cz>
2018-06-20 21:39:06 +0200

    Init

---
+++ src/TomLabs.Profiling.Tests/InlineProfilerTests.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TomLabs.Profiling.Tests
+{
+	[TestClass]
+	public class InlineProfilerTests
+	{
+		[TestMethod]
+		public void TestMethod()
+		{
+			// Setup write output
+			InlineProfiler.WriteTo(x => Debug.WriteLine(x));
+			// And another one if you want, with custom formating
+			InlineProfiler.WriteTo((type, label, elapsed) => Console.WriteLine($"|{type.ToString().ToUpper()}| Label:{label} - {elapsed}ms"));
+
+			InlineProfiler.Probe("Some label");
+
+			// Some long running code
+			Thread.Sleep(100);
+
+			// Get current timestamp
+			InlineProfiler.Probe();
+
+			// Or write an absolute time spent in this section
+			using (InlineProfiler.ProbeSection("Slow section"))
+			{
+				// Some very long running code :)
+				Thread.Sleep(1000);
+			}
+
+			InlineProfiler.Probe();
+		}
+	}
+}
---
+++ src/TomLabs.Profiling/InlineProfiler.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+namespace TomLabs.Profiling
+{
+	/// <summary>
+	/// Lightweigth class used for profiling code performace using timestamps.
+	/// </summary>
+	public class InlineProfiler : IDisposable
+	{
+		public enum ProfilerType : byte
+		{
+			/// <summary>
+			/// Measuring inline time
+			/// </summary>
+			Probe,
+			/// <summary>
+			/// Measuring wrapped section
+			/// </summary>
+			Section
+		}
+
+		/// <summary>
+		/// Profiler's Label
+		/// </summary>
+		public string Label { get; set; }
+
+		/// <summary>
+		/// Gets how much time was spend after instantiating <see cref="InlineProfiler"/>
+		/// </summary>
+		public long ElapsedMilliseconds => InstanceStopwatch.ElapsedMilliseconds;
+
+		protected Stopwatch InstanceStopwatch { get; set; }
+		protected long WatchStartTime { get; set; }
+		protected long SectionTime => InstanceStopwatch.ElapsedMilliseconds - WatchStartTime;
+
+		private static Stopwatch SWatch { get; set; }
+		private static int _probeOrder = 1;
+		private static int _sectionOrder = 1;
+
+		private static List<Action<string>> Sinks { get; set; } = new List<Action<string>>();
+		private static List<Action<ProfilerType, string, long>> StructuredSinks { get; set; } = new List<Action<ProfilerType, string, long>>();
+
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="watch"></param>
+		/// <param name="label"></param>
+		protected InlineProfiler(Stopwatch watch, string label = null)
+		{
+			Label = GetLabel(label, false);
+			InstanceStopwatch = watch ?? Stopwatch.StartNew();
+			WatchStartTime = InstanceStopwatch.ElapsedMilliseconds;
+			_sectionOrder++;
+		}
+		public void Dispose()
+		{
+			WriteLog(ProfilerType.Section, Label, SectionTime);
+		}
+
+		/// <summary>
+		/// Creates <see cref="InlineProfiler"/> instance that writes message with current timestamp in milliseconds after beeing disposed.
+		/// Note: Use with using statement
+		/// </summary>
+		/// <example>
+		/// using(InlineProfiler.ProbeSection("test"))
+		/// {
+		///		Thread.Sleep(1000);
+		/// }
+		/// </example>
+		/// <param name="label"></param>
+		/// <returns></returns>
+		public static InlineProfiler ProbeSection(string label = null)
+		{
+			return new InlineProfiler(SWatch, label);
+		}
+
+		/// <summary>
+		/// Creates probe message with current timestamp in milliseconds
+		/// </summary>
+		/// <param name="label"></param>
+		/// <param name="reset"></param>
+		public static void Probe(string label = null, bool reset = false)
+		{
+			if (reset)
+			{
+				Reset();
+			}
+
+			SWatch = SWatch ?? Stopwatch.StartNew();
+			WriteLog(ProfilerType.Probe, label, SWatch.ElapsedMilliseconds);
+			_probeOrder++;
+		}
+
+		/// <summary>
+		/// Resets profiler's <see cref="Stopwatch"/>
+		/// </summary>
+		public static void Reset()
+		{
+			if (SWatch != null)
+			{
+				SWatch.Stop();
+				SWatch.Reset();
+			}
+		}
+
+		/// <summary>
+		/// Sets write function that will be used for logging messages
+		/// </summary>
+		/// <param name="writeFunction">Function receiving <see cref="string"/> message parameter</param>
+		public static void WriteTo(Action<string> writeFunction)
+		{
+			Sinks.Add(writeFunction);
+		}
+
+		/// <summary>
+		/// Sets write function that will be used for logging messages
+		/// </summary>
+		/// <param name="writeFunction">Function receiving <see cref="ProfilerType"/>, <see cref="Label"/> and <see cref="ElapsedMilliseconds"/> parameters</param>
+		public static void WriteTo(Action<ProfilerType, string, long> writeFunction)
+		{
+			StructuredSinks.Add(writeFunction);
+		}
+
+		protected static void WriteLog(ProfilerType type, string label, long elapsed)
+		{
+			WriteLog($"{type.ToString().ToUpper()} {GetLabel(label)}: {elapsed}ms");
+
+			WriteStructuredLog(type, GetLabel(label), elapsed);
+		}
+
+		protected static void WriteLog(string message)
+		{
+			foreach (var sink in Sinks)
+			{
+				sink?.Invoke(message);
+			}
+		}
+
+		protected static void WriteStructuredLog(ProfilerType type, string label, long elapsed)
+		{
+			foreach (var sink in StructuredSinks)
+			{
+				sink?.Invoke(type, label, elapsed);
+			}
+		}
+
+		private static string GetLabel(string label = null, bool probe = true)
+		{
+			return label ?? (probe ? _probeOrder : _sectionOrder).ToString();
+		}
+	}
+}
